c326488fe13ef433bb3139880638b3bb0d4e62ab
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-03-10 13:37:15 +1300

    All for complied game v2

---
+++ 06_Help_GUI_v1.py
@@ -0,0 +1,191 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+import random
+
+
+class Start:
+    def __init__(self, parent):
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Mystery Heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=1)
+
+        # Help Button (row 2)
+        self.help_button = Button(self.start_frame, text="Help",
+                                  command=self.to_help)
+        self.help_button.grid(row=2, pady=10)
+
+    def to_help(self):
+        get_help = Help(self)
+
+
+class Help:
+    def __init__(self, partner):
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # Sets up child window (ie: help box)
+        self.help_box = Toplevel()
+
+        # If users press cross at top, closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # Set up GUI Frame
+        self.help_frame = Frame(self.help_box, width=300)
+        self.help_frame.grid()
+
+        # Set up Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Healp / Instructions",
+                                 font="arial 14 bold")
+        self.how_heading.grid(row=0)
+
+        help_text = "Choose an amount to play with and then choose the stakes. " \
+                    "Higher stakes cost more per round but you can win more as " \
+                    "well.\n\n" \
+                    "When you enter the play area, you will see three mystery " \
+                    "boxes. To reveal the contents of the boxes, click the  " \
+                    "'Open Boxes' button. If don't have enough money to play, " \
+                    "the button will turn red and you will need to quite the  " \
+                    "game.\n\n" \
+                    "The contents of the boxes will be added to your balance. " \
+                    "The boxes could contain...\n\n" \
+                    "Low: Lead ($0) | Copper ($1) | Silver ($2) | Gold ($10)\n" \
+                    "Medium: Lead ($0) | Copper ($2) | Silver ($4) | Gold ($25)\n" \
+                    "High: Lead ($0) | Copper ($5) | Silver ($10) | Gold ($50)\n\n" \
+                    "If each box contains gold, you earn $30 (low stakes). If " \
+                    "they contained copper, silver and gold, you would receive " \
+                    "$13 ($1 + $2 + $10) and so on."
+
+        # Help text (label, row 1)
+        self.help_text = Label(self.help_frame, text=help_text,
+                               justify=LEFT, wrap=400, padx=10, pady=10)
+        self.help_text.grid(row=1)
+
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg="#660000", fg="white",
+                                  font="arial 15 bold", )
+
+        # Entry box, Button & Error Label (row 2)
+        self.entry_error_frame = Frame(self.start_frame, width=200)
+        self.entry_error_frame.grid(row=2)
+
+        self.start_amount_entry = Entry(self.entry_error_frame,
+                                        font="Arial 19 bold", width=10)
+        self.start_amount_entry.grid(row=0, column=0)
+
+        self.add_funds_button = Button(self.entry_error_frame,
+                                       font="Arial 14 bold",
+                                       text="Add Funds",
+                                       command=self.check_funds)
+        self.add_funds_button.grid(row=0, column=1)
+
+        self.amount_error_label = Label(self.entry_error_frame, fg="maroon",
+                                        text="", font="Arial 10 bold", wrap=275,
+                                        justify=LEFT)
+        self.amount_error_label.grid(row=1, columnspan=2, pady=5)
+
+        # button frame (row 3)
+        self.stakes_frame = Frame(self.start_frame)
+        self.stakes_frame.grid(row=3)
+
+        # Buttons go here...
+        button_font = "Arial 12 bold"
+        # Orange low stakes button...
+        self.low_stakes_button = Button(self.stakes_frame, text="Low ($5) ",
+                                        command=lambda: self.to_game(1),
+                                        font=button_font, bg="#FF9933")
+        self.low_stakes_button.grid(row=0, column=0, pady=10)
+
+        # Yellow medium stakes button...
+        self.medium_stakes_button = Button(self.stakes_frame, text="Medium ($10)",
+                                           command=lambda: self.to_game(2),
+                                           font=button_font, bg="#FFFF33")
+        self.medium_stakes_button.grid(row=0, column=1, padx=5, pady=10)
+
+        # Green high stakes button...
+        self.high_stakes_button = Button(self.stakes_frame, text="High ($15)",
+                                         command=lambda: self.to_game(3),
+                                         font=button_font, bg="#99FF33")
+        self.high_stakes_button.grid(row=0, column=2, pady=10)
+
+        # Disabled all stakes buttons at start
+        self.low_stakes_button.config(state=DISABLED)
+        self.medium_stakes_button.config(state=DISABLED)
+        self.high_stakes_button.config(state=DISABLED)
+
+    def check_funds(self):
+        starting_balance = self.start_amount_entry.get()
+
+        # Set error background colours (and assume that thare are no
+        # errors at the start...
+        error_back = "#ffafaf"
+        has_errors = "no"
+        error_feedback = ""
+
+        # change background to white (for testing purposes) ...
+        self.start_amount_entry.config(bg="white")
+        self.amount_error_label.config(text="")
+
+        # Disable all stakes buttons in case user changes mind and
+        # decreases amount entered.
+        self.low_stakes_button.config(state=DISABLED)
+        self.medium_stakes_button.config(state=DISABLED)
+        self.high_stakes_button.config(state=DISABLED)
+
+        try:
+            starting_balance = int(starting_balance)
+
+            if starting_balance < 5:
+                has_errors = "yes"
+                error_feedback = "Sorry, the least you can play with is $5"
+            elif starting_balance > 50:
+                has_errors = "yes"
+                error_feedback = "Too high! The most you can risk in this " \
+                                 "game is $50"
+
+            elif starting_balance >= 15:
+                # enable all buttons
+                self.low_stakes_button.config(state=NORMAL)
+                self.medium_stakes_button.config(state=NORMAL)
+                self.high_stakes_button.config(state=NORMAL)
+            elif starting_balance >= 10:
+                # enable low and medium stakes buttons
+                self.low_stakes_button.config(state=NORMAL)
+                self.medium_stakes_button.config(state=NORMAL)
+            else:
+                self.low_stakes_button.config(state=NORMAL)
+
+        except ValueError:
+            has_errors = "yes"
+            error_feedback = "Please enter a dollar amount (no text / decimals)"
+
+        if has_errors == "yes":
+            self.start_amount_entry.config(bg=error_back)
+            self.amount_error_label.config(text=error_feedback)
+        else:
+            # Set starting balance to amount entered by user
+            self.starting_funds.set(starting_balance)
+
+    def to_game(self, stakes):
+
+        # retrieve starting balance
+        starting_balance = self.starting_funds.get()
+
+        Game(self, stakes, starting_balance)
+
+        # hide start up window
+        root.withdraw()
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box Game")
+    something = Start(root)
+    root.mainloop()
